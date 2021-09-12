ab1ba6b8e042d576c0c94b7820bb7ec6c0f38f01
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-12 14:07:35 -0500

    commit tree 0aa7c9e1cdd87c54e1e55e83ea9d53d2e7a7120f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record){
+    let retVal = record.find(({result}) => result === 'W');
+    if(retVal){
+        return retVal.year;
+    }else{
+        return retVal;
+    }
+}
